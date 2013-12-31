620d3a8ec3f692af6b95c39acd76a1d95e11c89c
brianbilbro_cp <SND\brianbilbro_cp>
2013-12-31 12:47:55 +0000

    Saves both the firstcharacter.xml and charactername.xml file now

--- The Darkest Hour/The Darkest Hour/LoadSave.cs
+++ The Darkest Hour/The Darkest Hour/LoadSave.cs
@@ -62,6 +62,12 @@ namespace The_Darkest_Hour
         /// </notes>
         private static void SaveToXmlFile(Player myHero)
         {
+            SaveToXmlFile(myHero, "FirstCharacter.xml");
+            SaveToXmlFile(myHero, GetCharacterFileName(myHero));
+        }
+
+        private static void SaveToXmlFile(Player myHero, string fileName)
+        {
             if (Directory.Exists(GameConfigs.PlayerGameFilesLocation) == false)
             {
                 Directory.CreateDirectory(GameConfigs.PlayerGameFilesLocation);
@@ -72,7 +78,7 @@ namespace The_Darkest_Hour
             //System.Xml.Serialization.XmlSerializer playerXmlSerialization = new System.Xml.Serialization.XmlSerializer(typeof(Player), knownTypes);
             System.Xml.Serialization.XmlSerializer playerXmlSerialization = new System.Xml.Serialization.XmlSerializer(typeof(GameObject));
 
-            using (System.IO.StreamWriter characterStreamWriter = new System.IO.StreamWriter(Path.Combine(GameConfigs.PlayerGameFilesLocation, "FirstCharacter.xml")))
+            using (System.IO.StreamWriter characterStreamWriter = new System.IO.StreamWriter(Path.Combine(GameConfigs.PlayerGameFilesLocation, fileName)))
             {
                 playerXmlSerialization.Serialize(characterStreamWriter, myHero);
                 characterStreamWriter.Close();
@@ -80,6 +86,20 @@ namespace The_Darkest_Hour
                 Console.WriteLine("Game saved to xml file successfully\n");
             }
 
+        }
+
+        public static string GetCharacterFileName(Player myHero)
+        {
+            StringBuilder returnData = new StringBuilder();
+
+            char[] arr = myHero.Identifier.ToCharArray();
+
+            arr = Array.FindAll<char>(arr, (c => (char.IsLetterOrDigit(c))));
+
+            returnData.Append(arr);
+            returnData.Append(".xml");
+
+            return returnData.ToString();
         }
     }
 }
