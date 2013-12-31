0eb737aa6b6790f127c3a5d057d956bde1a8cb4a
brianbilbro_cp <SND\brianbilbro_cp>
2013-12-31 00:57:59 +0000

    Arena Action implemented

--- The Darkest Hour/The Darkest Hour/Locations/Actions/ArenaAction.cs
+++ The Darkest Hour/The Darkest Hour/Locations/Actions/ArenaAction.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using The_Darkest_Hour.Characters.Mobs;
+using The_Darkest_Hour.Characters;
 
 namespace The_Darkest_Hour.Locations.Actions
 {
@@ -18,7 +20,24 @@ namespace The_Darkest_Hour.Locations.Actions
         {
             Location returnData = GameState.CurrentLocation;
 
-            Console.WriteLine("Arena not implemented yet.");
+            Battle battle = new Battle();
+            Mob mob;
+            Random rand = new Random();
+            int temp = 0;
+
+            temp = rand.Next(1, 3);
+            if (temp == 1)
+            {
+                mob = new Skeleton();
+            }
+            else
+            {
+                mob = new Bandit();
+            }
+
+            Console.Clear();
+            battle.DoBattle(GameState.Hero, mob);
+            GameState.Hero.ResetHealth();
 
             this.ClearScreen();
 
