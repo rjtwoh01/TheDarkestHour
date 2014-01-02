9242d18e7878424a58631f6400832f7a6ef0a151
brianbilbro_cp <SND\brianbilbro_cp>
2014-01-02 13:11:43 +0000

    Location State handler methods

--- The Darkest Hour/The Darkest Hour/Locations/LocationHandler.cs
+++ The Darkest Hour/The Darkest Hour/Locations/LocationHandler.cs
@@ -9,7 +9,48 @@ namespace The_Darkest_Hour.Locations
     class LocationHandler
     {
 
+        #region Location State Static Methods
+
+        public static bool LocationStateExists(string locationStateKey)
+        {
+            return GameState.GameLocations.ContainsKey(locationStateKey);
+        }
+
+        public static void AddLocationState(string locationStateKey, LocationState locationState)
+        {
+            if (LocationHandler.LocationExists(locationStateKey))
+            {
+                LocationHandler.RemoveLocation(locationStateKey);
+            }
+
+            GameState.GameLocationStates.Add(locationStateKey, locationState);
+        }
+
+        public static LocationState GetLocationState(string locationStateKey)
+        {
+            LocationState returnData;
+
+            if (LocationStateExists(locationStateKey))
+            {
+                returnData = GameState.GameLocationStates[locationStateKey];
+            }
+            else
+            {
+                // TODO: When saving Game Location States is implemented
+                // then this method should check if the game location state
+                // saved file exists.  If so, it should load the file
+                // otherwise it should create a new LocationStates file.
+                returnData = new LocationState();
+                GameState.GameLocationStates.Add(locationStateKey, returnData);
+            }
+
+            return returnData;
+        }
+
+        #endregion
+
         #region Location Static Methods
+
         public static void ResetLocation(LocationDefinition locationDefinition)
         {
             ResetLocation(locationDefinition.LocationKey);
