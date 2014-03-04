7f4dc5a3e35fd8f9ec62d9c86eb34e7888f1a17a
enyahs95_cp <SND\enyahs95_cp>
2014-03-04 16:36:02 +0000

    Beginning of Windows Form development and Design

---
+++ The Darkest Hour/The Darkest Hour/Windows.UI/DarkestHourWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using The_Darkest_Hour.Characters;
+using The_Darkest_Hour.Characters.Professions;
+using The_Darkest_Hour.Items;
+using The_Darkest_Hour.Locations;
+
+namespace CodeReference
+{
+    public partial class DarkestHourWindow : Form
+    {
+        public DarkestHourWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
